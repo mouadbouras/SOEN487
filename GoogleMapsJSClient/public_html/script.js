21d812246f2957126257a7403d4dfbdd0f5e3891
mouad bouras <mouad bouras>
2017-04-06 20:14:51 -0400

    Added marty's last fm account, cleaned up some code

--- GoogleMapsJSClient/public_html/script.js
+++ GoogleMapsJSClient/public_html/script.js
@@ -7,33 +7,33 @@
 
         var map;
         
-        var markers =[{ name: "Mouad Bouras" , label : "B" , lat : 45.5093356 , lng : -73.677426, twitter : "mouadbouras" , spotify:"mouadbouras"} ,
-                      { name: "Concordia " , label : "S" , lat : 45.492261 , lng : -73.5783127, twitter :"concordia"  , spotify:""},
-                      { name: "Sean Canlas" , label : "G" , lat : 45.4609075 , lng : -73.6278422, twitter :"seancanlas" , spotify:""},
-                      {name: "Trisha Dimayuga" , label : "D" , lat: 45.4729346, lng: -73.6552988, twitter :"trishapoynter"  , spotify:""},
-                      { name: "Mike Cabana" , label:"C"   , lat: 45.604174, lng: -73.6724567, twitter :"mikecabana" , spotify:""}, 
-                      { name: "Martin Nafekh" , label:"N"   , lat: 45.4842901, lng: -73.8476348, twitter :""  , spotify:""}, 
-                      {name: "Aakash Patel" , label:"P"   , lat: 45.5709407, lng: -73.7029947, twitter : "" , spotify:""}]; 
+        var markers =[{ name: "Mouad Bouras" , label : "B" , lat : 45.5093356 , lng : -73.677426, twitter : "mouadbouras" , lastFm:"mouadbouras"} ,
+                      { name: "Concordia " , label : "S" , lat : 45.492261 , lng : -73.5783127, twitter :"concordia"  , lastFm:""},
+                      { name: "Sean Canlas" , label : "G" , lat : 45.4609075 , lng : -73.6278422, twitter :"seancanlas" , lastFm:""},
+                      { name: "Trisha Dimayuga" , label : "D" , lat: 45.4729346, lng: -73.6552988, twitter :"trishapoynter"  , lastFm:""},
+                      { name: "Mike Cabana" , label:"C"   , lat: 45.604174, lng: -73.6724567, twitter :"mikecabana" , lastFm:""}, 
+                      { name: "Martin Nafekh" , label:"N"   , lat: 45.4842901, lng: -73.8476348, twitter :""  , lastFm:"cynykul"}, 
+                      {name: "Aakash Patel" , label:"P"   , lat: 45.5709407, lng: -73.7029947, twitter : "" , lastFm:""}]; 
         
         function initMap() {
-            var infowindow = new google.maps.InfoWindow();
+            var infowindow = new google.maps.InfoWindow({maxWidth: 400});
             map = new google.maps.Map(document.getElementById('map'), {
               center: {lat: 45.5425158, lng: -73.6378987},
               zoom: 11
             });
-            
+
             map.addListener('click', function(event) {
                 var latitude = event.latLng.lat();
                 var longitude = event.latLng.lng();
                 
                 infowindow.setPosition(event.latLng);
-                infowindow.setContent("<h5>New Marker : </h5>" +
+                infowindow.setContent("<div class='container-fluid'><h5>New Marker : </h5>" +
                                       "" +
                                       "<div class='row' style='width:245px; margin-top:5px'><div class='col-xs-4' >Name :</div><div class='col-xs-8' style='margin-top:5px'> <input id='markerName' style='width:132px' type='text'> </div></div>" +
                                       "<div class='row' style='width:245px;margin-top:5px'><div class='col-xs-4' >Twitter :</div><div class='col-xs-8' style='margin-top:5px'> <input id='markerTwitter' style='width:132px' type='text'></div> </div>" +
-                                      "<div class='row' style='width:245px; margin-top:5px'><div class='col-xs-4' >Spotify :</div><div class='col-xs-8' style='margin-top:5px'> <input id='markerSpotify' style='width:132px' type='text'> </div></div>" + 
+                                      "<div class='row' style='width:245px; margin-top:5px'><div class='col-xs-4' >LastFm :</div><div class='col-xs-8' style='margin-top:5px'> <input id='markerlastFm' style='width:132px' type='text'> </div></div>" + 
                                       "<div class='row' style='width:245px; margin-top:5px'><div class='col-xs-12 text-right'><button id='markerButton' type='button' class='btn btn-default btn-sm'> Add </button></div></div>" +
-                                      "");
+                                      "</div>");
                 infowindow.open(map);
                 
                 $('#markerButton').click(function(){
@@ -41,21 +41,14 @@
                 var marker = { name: $('#markerName').val() , 
                                label : $('#markerName').val().trim().charAt(0).toUpperCase() , 
                                lat : latitude , lng : longitude, 
-                               twitter : $('#markerTwitter').val() , spotify : $('#markerSpotify').val()};
+                               twitter : $('#markerTwitter').val() , lastFm : $('#markerlastFm').val()};
                 placeMarker( marker );          
                 infowindow.close();
 
 
                 });
 
-                
-
-                
-                
-          });
-
-          
-
+            });
 
             function placeMarker( mymarker ) {
                 var marker = new google.maps.Marker({
@@ -65,7 +58,7 @@
                 });
 
                 marker.addListener('click', function() {
-                    getUserTweets(mymarker.twitter);
+                    getUserTweets(mymarker.twitter, mymarker.lastFm);
                 });  
                 
                 marker.addListener('mouseover', function() {
@@ -85,40 +78,73 @@
             }
         }
         
-        function getUserTweets(twitterName)
+        function getUserTweets(twitterName,lastFm)
         {
-            console.log(twitterName);
+            $("#twitter").html("");
+            $("#lastFm").html("");                
+            console.log(lastFm);
             $.ajax({
-                url: 'https://arcane-wave-75994.herokuapp.com/webresources/mashedservice/'+twitterName +'/spotify',
+                url: 'https://soen487-server.herokuapp.com/webresources/mashedservice/twitter/'+twitterName,
                 type: 'GET',
                 dataType: "json",
                 processData: false,
                 success: function(data) { 
                     
-                    console.log(data["twiits"]); 
-                    var output = "<div class='user-info'><h3>" + data.twiits.username + "</h3>" + 
-                                 "<h3><small>" + data.twiits.description + "</small></h3></div>";
-                    for(var i = 0 ; i < data.twiits.tweets.length ; i++)
+                    console.log(data); 
+                    var output = "<div class='user-info'><h3>" + data.username + "</h3>" + 
+                                 "<h3><small>" + data.description + "</small></h3></div>";
+                    for(var i = 0 ; i < data.tweets.length ; i++)
                     {
-                        output += "<div class='tweet'><p>" + data.twiits.tweets[i].text + 
-                                    "<br><br>" + data.twiits.tweets[i].date + 
-                                    " Likes : " + data.twiits.tweets[i].likes + 
-                                    " Retweets : " + data.twiits.tweets[i].retweets +  
+                        output += "<div class='tweet'><p>" + data.tweets[i].text + 
+                                    "<br><br>" + data.tweets[i].date + 
+                                    " Likes : " + data.tweets[i].likes + 
+                                    " Retweets : " + data.tweets[i].retweets +  
                                 "</p></div>";
                     }
-                         console.log(data.twiits.length);
+                         console.log(data.length);
                     $("#twitter").html(output);
                 },
                 error:  function(data) { 
                     $("#twitter").html("<div class='user-info'><h2>Nothing found</h2></div>");
+                }
+            }); 
+            
+            $.ajax({
+                url: 'https://soen487-server.herokuapp.com/webresources/mashedservice/lastfm/'+lastFm,
+                type: 'GET',
+                dataType: "json",
+                processData: false,
+                success: function(data) { 
+                   
+                    var output = "<div class='user-info'><h3>" + data.user + "</h3> <img class='LastFmImg' src='" + data.profileImage + "' alt='Profile' >" + 
+                                 "<h3><small>" + data.displayName + "</small></h3>\n\
+                                  <h3><small>Scrobles : " + data.scrobles + "</small></h3></div>";
+                    for(var i = 0 ; i < data.songs.length ; i++)
+                    {
+                        output += "<div class='tweet'><p>" + data.songs[i].name + 
+                                    " - " + data.songs[i].artist + 
+                                    " <br> " + data.songs[i].album + 
+                                    " <br><br>" + data.songs[i].date +                                     
+                                "</p></div>";
+                    }
+                    $("#lastFm").html(output);                    
+                    
+                },
+                error:  function(data) { 
+                    $("#lastFm").html("<div class='user-info'><h2>Nothing found</h2></div>");                   
 
                 }
                   
                 
-            }); 
+            });             
             
         }        
-$(function() {            
-
-
-});
+        
+var $loading = $('.spinner').hide();
+$(document)
+  .ajaxStart(function () {
+    $loading.show();
+  })
+  .ajaxStop(function () {
+    $loading.hide();
+  });
