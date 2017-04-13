b13cebdb4f0f9eac8521422c7c9ba398b49a0c09
mouad bouras <mouad bouras>
2017-04-13 00:00:05 -0400

    fixed up some bugs

--- GoogleMapsJSClient/public_html/script.js
+++ GoogleMapsJSClient/public_html/script.js
@@ -6,17 +6,21 @@
 
 
         var map;
+        var infowindow;
+        var markers = []; 
+        $('#success').hide();
+        $('#error').hide();
         
-        var markers =[{ name: "Mouad Bouras" , label : "B" , lat : 45.5093356 , lng : -73.677426, twitter : "mouadbouras" , lastFm:"mouadbouras"} ,
-                      { name: "Concordia " , label : "S" , lat : 45.492261 , lng : -73.5783127, twitter :"concordia"  , lastFm:""},
-                      { name: "Sean Canlas" , label : "G" , lat : 45.4609075 , lng : -73.6278422, twitter :"seancanlas" , lastFm:""},
-                      { name: "Trisha Dimayuga" , label : "D" , lat: 45.4729346, lng: -73.6552988, twitter :"trishapoynter"  , lastFm:""},
-                      { name: "Mike Cabana" , label:"C"   , lat: 45.604174, lng: -73.6724567, twitter :"mikecabana" , lastFm:""}, 
-                      { name: "Martin Nafekh" , label:"N"   , lat: 45.4842901, lng: -73.8476348, twitter :""  , lastFm:"cynykul"}, 
-                      {name: "Aakash Patel" , label:"P"   , lat: 45.5709407, lng: -73.7029947, twitter : "" , lastFm:""}]; 
+//            [{ name: "Mouad Bouras" , label : "B" , lat : 45.5093356 , lng : -73.677426, twitter : "mouadbouras" , lastFm:"mouadbouras"} ,
+//                      { name: "Concordia " , label : "S" , lat : 45.492261 , lng : -73.5783127, twitter :"concordia"  , lastFm:""},
+//                      { name: "Sean Canlas" , label : "G" , lat : 45.4609075 , lng : -73.6278422, twitter :"seancanlas" , lastFm:""},
+//                      { name: "Trisha Dimayuga" , label : "D" , lat: 45.4729346, lng: -73.6552988, twitter :"trishapoynter"  , lastFm:""},
+//                      { name: "Mike Cabana" , label:"C"   , lat: 45.604174, lng: -73.6724567, twitter :"mikecabana" , lastFm:""}, 
+//                      { name: "Martin Nafekh" , label:"N"   , lat: 45.4842901, lng: -73.8476348, twitter :""  , lastFm:"cynykul"}, 
+//                      {name: "Aakash Patel" , label:"P"   , lat: 45.5709407, lng: -73.7029947, twitter : "" , lastFm:""}]; 
         
         function initMap() {
-            var infowindow = new google.maps.InfoWindow({maxWidth: 400});
+            infowindow = new google.maps.InfoWindow({maxWidth: 400});
             map = new google.maps.Map(document.getElementById('map'), {
               center: {lat: 45.5425158, lng: -73.6378987},
               zoom: 11
@@ -37,28 +41,59 @@
                 infowindow.open(map);
                 
                 $('#markerButton').click(function(){
-
-                var marker = { name: $('#markerName').val() , 
-                               label : $('#markerName').val().trim().charAt(0).toUpperCase() , 
+                var $name = $('#markerName').val();
+                var $twitter = $('#markerTwitter').val();
+                var $lastfm = $('#markerlastFm').val();
+                console.log($name + $twitter +$lastfm );
+                var marker = { name: $name , 
+                               label : $name.trim().charAt(0).toUpperCase() , 
                                lat : latitude , lng : longitude, 
-                               twitter : $('#markerTwitter').val() , lastFm : $('#markerlastFm').val()};
-                placeMarker( marker );          
-                infowindow.close();
-
-
+                               twitter : $twitter , lastfm : $lastfm };
+                
+                if ($name.trim() == ''|| $twitter.trim() == '' || $lastfm.trim() =='' )
+                {                
+                    $('#error').fadeIn();
+                   setTimeout(function(){
+                          $('#error').fadeOut();
+                         },2000);   
+                    return;
+                }
+                $.ajax({
+                url: 'https://soen487-server.herokuapp.com/webresources/mashedservice/addUser/' + 
+                      $name +'/'+  $name.trim().charAt(0).toUpperCase() + '/' + latitude + '/' + longitude+ '/' + $twitter + '/' + $lastfm
+                ,
+                type: 'POST',
+                success: function(data) { 
+                    $('#success').fadeIn();
+                         setTimeout(function(){
+                          $('#success').fadeOut();
+                         },2000);                    
+                    placeMarker( marker );          
+                    infowindow.close();
+                },
+                error:  function(data) { 
+                    $('#error').fadeIn();
+                   setTimeout(function(){
+                          $('#error').fadeOut();
+                         },2000);                      
+                }
+            }); 
                 });
 
+
+            
+            
             });
 
             function placeMarker( mymarker ) {
                 var marker = new google.maps.Marker({
-                    position: {lat: mymarker.lat, lng : mymarker.lng},
+                    position: {lat: parseFloat(mymarker.lat), lng : parseFloat(mymarker.lng)},
                     map: map,
                     label: mymarker.label,                 
                 });
 
                 marker.addListener('click', function() {
-                    getUserTweets(mymarker.twitter, mymarker.lastFm);
+                    getUserTweets(mymarker.twitter, mymarker.lastfm);
                 });  
                 
                 marker.addListener('mouseover', function() {
@@ -71,11 +106,30 @@
                 });                     
                 
             }
+            
+              $.ajax({
+                url: 'https://soen487-server.herokuapp.com/webresources/mashedservice/getUsers',
+                type: 'GET',
+                dataType: "json",
+                processData: false,
+                beforeSend: function(){
+                    $('.grey-spinner').show();
+                },
+                complete: function(){
+                    $('.grey-spinner').hide();
+                },                
+                success: function(data) { 
+                    for(var i = 0 ; i < data.length ; i++)
+                    {
+                        placeMarker( data[i] );
+                    }
+                },
+                error:  function(data) { 
+                    $("#twitter").html("<div class='user-info'><h2>Nothing found</h2></div>");
+                }
+            }); 
 
-            for(var i = 0 ; i < markers.length ; i++)
-            {
-                placeMarker( markers[i] );
-            }
+
         }
         
         function getUserTweets(twitterName,lastFm)
@@ -88,6 +142,12 @@
                 type: 'GET',
                 dataType: "json",
                 processData: false,
+                beforeSend: function(){
+                    $('.blue-spinner').show();
+                },
+                complete: function(){
+                    $('.blue-spinner').hide();
+                },
                 success: function(data) { 
                     
                     console.log(data); 
@@ -114,21 +174,35 @@
                 type: 'GET',
                 dataType: "json",
                 processData: false,
+                beforeSend: function(){
+                    $('.red-spinner').show();
+                },
+                complete: function(){
+                    $('.red-spinner').hide();
+                },                
                 success: function(data) { 
-                   
+                    if(data.displayName == ""){
+                        $("#lastFm").html("<div class='user-info'><h2>Nothing found</h2></div>"); 
+                        return;
+                    }
                     var output = "<div class='user-info'><h3>" + data.user + "</h3> <img class='LastFmImg' src='" + data.profileImage + "' alt='Profile' >" + 
                                  "<h3><small>" + data.displayName + "</small></h3>\n\
                                   <h3><small>Scrobles : " + data.scrobles + "</small></h3></div>";
-                    for(var i = 0 ; i < data.songs.length ; i++)
-                    {
-                        output += "<div class='tweet'><p>" + data.songs[i].name + 
-                                    " - " + data.songs[i].artist + 
-                                    " <br> " + data.songs[i].album + 
-                                    " <br><br>" + data.songs[i].date +                                     
-                                "</p></div>";
+                    
+                    if(data.songs != undefined){
+                        for(var i = 0 ; i < data.songs.length ; i++)
+                        {
+                            output += "<div class='tweet'><p>" + data.songs[i].name + 
+                                        " - " + data.songs[i].artist + 
+                                        " <br> " + data.songs[i].album + 
+                                        " <br><br>" + data.songs[i].date +                                     
+                                    "</p></div>";
+                        }
+                    }else{
+                        output += "<div class='tweet'><p>No Songs Found!</p></div>";
                     }
                     $("#lastFm").html(output);                    
-                    
+
                 },
                 error:  function(data) { 
                     $("#lastFm").html("<div class='user-info'><h2>Nothing found</h2></div>");                   
@@ -141,10 +215,10 @@
         }        
         
 var $loading = $('.spinner').hide();
-$(document)
-  .ajaxStart(function () {
-    $loading.show();
-  })
-  .ajaxStop(function () {
-    $loading.hide();
-  });
+//$(document)
+//  .ajaxStart(function () {
+//    $loading.show();
+//  })
+//  .ajaxStop(function () {
+//    $loading.hide();
+//  });
